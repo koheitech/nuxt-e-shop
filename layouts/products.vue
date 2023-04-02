370935fdf2d5af2f5bab58345848fd4dd17da43d
koheitech <koheitechigh@gmail.com>
2023-04-02 15:58:51 +0300

    add custom layout

---
+++ layouts/products.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/products">Nuxt E-Shop Top</NuxtLink>
+            </nav>
+        </header>
+
+        <div><slot /></div>
+
+        <footer>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/products">Products</NuxtLink></li>
+            </ul>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+  .router-link-exact-active {
+    color: #12b488;
+  }
+</style>
