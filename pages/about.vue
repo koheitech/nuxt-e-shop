9590099f76c37fa2fcf3fd398b9556de679a9678
koheitech <koheitechigh@gmail.com>
2023-04-02 15:23:45 +0300

    add pages and routes

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi esse odit, magnam tempore quam fugit. Laudantium vero fugit voluptate, suscipit ex quaerat ullam, explicabo neque sapiente consequatur obcaecati? Dolor?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi esse odit, magnam tempore quam fugit. Laudantium vero fugit voluptate, suscipit ex quaerat ullam, explicabo neque sapiente consequatur obcaecati? Dolor?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
