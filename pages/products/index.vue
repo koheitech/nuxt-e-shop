42b19244afd5d67e0c698f7ae89af94e616a3490
koheitech <koheitechigh@gmail.com>
2023-04-02 18:01:51 +0300

    create components for /products page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="grid grid-cols-4 gap-5">
             <div v-for="p in products">
-                <NuxtLink :to="`/products/${p.id}`">{{  p.title  }}</NuxtLink>
+                <ProductCard :product="p"/>
             </div>
         </div>
     </div>
@@ -16,7 +16,7 @@
     // fetch the products
     const { data: products } = await useFetch('https://fakestoreapi.com/products')
 </script>
-
+ 
 <style scoped>
 
 </style>
