47f9a0d939ba3e5e5138085c495c4fbb738da09e
koheitech <koheitechigh@gmail.com>
2023-04-02 15:31:16 +0300

    create route parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero laboriosam corrupti exercitationem magnam quae! Blanditiis beatae, sint laboriosam, ratione perferendis libero praesentium fugit sapiente quidem id sit, delectus officiis nesciunt.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
