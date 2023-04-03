93df6c16c90a7b1e9fed6e10d9504da09e5eee79
koheitech <koheitechigh@gmail.com>
2023-04-03 06:07:10 +0300

    throwing a custom error

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -14,6 +14,12 @@
     // Nuxt does not fetch new data to save the SSR by default
     // Thus, we need to explicitly set key as the second arguments to fetch new data for every time
     const { data: product } = await useFetch(uri, { key: id })
+
+    // We cannot catch error since we cannot know the error until we know that we could not fetch the data
+    // In this case, we can check the result in backend and throw a custom error
+    if (!product.value) {
+        throw createError({ statusCode: 404, statusMessage: 'Product not found'})
+    }
 </script>
 
 <style scoped>
