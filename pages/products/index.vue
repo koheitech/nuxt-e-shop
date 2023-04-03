e8b21caa7f1d29f6cba5d10cfb7b76dcb16302f0
koheitech <koheitechigh@gmail.com>
2023-04-03 06:22:22 +0300

    override meta data per page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -15,6 +15,13 @@
 
     // fetch the products
     const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+    useHead({
+        title: 'Nuxt E-shop | Products page',
+        meta: [
+            { name: 'description', content: 'Nuxt E-shop products top page'}
+        ]
+    })
 </script>
  
 <style scoped>
