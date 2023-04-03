828ad860209997840d9277fc9003af0444380660
koheitech <koheitechigh@gmail.com>
2023-04-03 06:37:37 +0300

    create server-route

--- pages/about.vue
+++ pages/about.vue
@@ -3,11 +3,12 @@
         <h2>About</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi esse odit, magnam tempore quam fugit. Laudantium vero fugit voluptate, suscipit ex quaerat ullam, explicabo neque sapiente consequatur obcaecati? Dolor?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi esse odit, magnam tempore quam fugit. Laudantium vero fugit voluptate, suscipit ex quaerat ullam, explicabo neque sapiente consequatur obcaecati? Dolor?</p>
+        <div>{{ data }}</div>
     </div>
 </template>
 
 <script setup>
-
+    const { data } = await useFetch('/api/eshop')
 </script>
 
 <style scoped>
